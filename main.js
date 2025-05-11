f14e485280851955a2f3bfc80ab71b3746b7d5ae
unknown <khaledalmutairi220@hotmail.com>
2025-05-11 13:14:07 +0300

    lab

---
+++ main.js
@@ -0,0 +1,15 @@
+ setInterval(() => 
+fetch('https://coffee.alexflipnote.dev/random.json')
+.then((res) => res.json())
+.then((date) => {
+
+    let continer = document.getElementById("continer");
+        let imgAPI = document.createElement('img');
+        
+        imgAPI.src = date.file
+        continer.innerText = " " 
+        continer.appendChild(imgAPI)
+    }),1000);
+    
+
+
